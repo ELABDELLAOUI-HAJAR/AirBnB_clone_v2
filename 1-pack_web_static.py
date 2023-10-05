9205c61946286468f35d64b1908736a96ea4467c
Hajar <elabdellaoui.hajar@gmail.com>
2023-10-05 03:45:07 -0700

    fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,16 +5,15 @@ from fabric.api import local
 from datetime import datetime
 
 
-@task
 def do_pack():
-""" generates a .tgz archive from the contents of
-the web_static folder of your AirBnB Clone repo
-"""
+    """ generates a .tgz archive from the contents of
+    the web_static folder of your AirBnB Clone repo
+    """
 
     local("mkdir -p versions")
     dformat = "%Y%m%d%H%M%S"
-    archive_path = "versions/web_static_{}.tgz"
-                    .format(datetime.strftime(datetime.now(), dformat))
+    archive_path = "versions/web_static_{}.tgz".format(
+            datetime.strftime(datetime.now(), dformat))
     result = local("tar -cvzf {} web_static".format(archive_path))
     if result.failed:
         return None
