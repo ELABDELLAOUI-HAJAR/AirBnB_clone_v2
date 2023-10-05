e441007034182a8363f58f0eafa01d205c68ce40
Hajar <elabdellaoui.hajar@gmail.com>
2023-10-05 07:31:27 -0700

    add runs_once decorator to do_pack function

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,6 +4,7 @@
 from fabric.api import local, env, put, run
 from datetime import datetime
 from os import path
+from fabric.decorators import runs_once
 
 
 env.hosts = ['54.237.65.38', '54.237.210.238']
@@ -15,6 +16,7 @@ env.user = "ubuntu"
 env.key_filename = "~/.ssh/id_rsa"
 
 
+@runs_once
 def do_pack():
     """ generates a .tgz archive from the contents of
     the web_static folder of your AirBnB Clone repo
