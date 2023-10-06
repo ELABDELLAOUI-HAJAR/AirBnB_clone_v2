b6ee033d148756ceb75b6b54e95c20e4e149c817
HAJAR EL ABDELLAOUI <125801785+ELABDELLAOUI-HAJAR@users.noreply.github.com>
2023-10-06 10:49:36 +0100

    fix 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,30 @@
 # Sets up your web servers for the deployment of web_static
 
-exec { 'server setup':
-  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo service nginx start && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i \'47i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\' /etc/nginx/sites-available/default && sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default && sudo service nginx restart',
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package { 'nginx':
+  ensure => installed,
+}
+-> exec { 'create directories':
+  command => '/usr/bin/mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"',
+}
+-> exec { 'test content':
+  command => '/usr/bin/echo "<h1>Welcome to hajaralx.tech site</h1>" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
+}
+-> exec { 'remove link':
+  command => '/usr/bin/rm -rf /data/web_static/current',
+}
+-> exec { 'create symbolic link':
+  command => '/usr/bin/ln -s /data/web_static/releases/test/ /data/web_static/current',
+}
+-> exec { 'grants':
+  command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
+}
+-> exec { 'hbnb_static location':
+  command => 'sudo sed -i "/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/;index index.html;}" /etc/nginx/sites-enabled/default',
   provider => shell,
+}
+-> exec { 'restart':
+  command => '/usr/sbin/service nginx restart',
 }
